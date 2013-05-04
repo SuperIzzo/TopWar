9ec3f37201ba7fcc1940aafe48b14780c5d22c9d
SuperIzzo <Hristoz2004@gmail.com>
2013-05-04 17:23:47 +0100

    conf.lua added and tests are now triggered by a flag Moved lunatest declarations to TestMain

--- main.lua
+++ main.lua
@@ -1,46 +1,30 @@
 require 'src.strict'
 
--- lunatest dependencies
-declare 'random'
-declare 'fail'
-declare 'skip'
-declare 'assert_true'
-declare 'assert_false'
-declare 'assert_nil'
-declare 'assert_not_nil'
-declare 'assert_equal'
-declare 'assert_not_equal'
-declare 'assert_gt'
-declare 'assert_lt'
-declare 'assert_gte'
-declare 'assert_lte'
-declare 'assert_len'
-declare 'assert_not_len'
-declare 'assert_match'
-declare 'assert_not_match'
-declare 'assert_boolean'
-declare 'assert_not_boolean'
-declare 'assert_number'
-declare 'assert_not_number'
-declare 'assert_string'
-declare 'assert_not_string'
-declare 'assert_table'
-declare 'assert_not_table'
-declare 'assert_function'
-declare 'assert_not_function'
-declare 'assert_thread'
-declare 'assert_not_thread'
-declare 'assert_userdata'
-declare 'assert_not_userdata'
-declare 'assert_metatable'
-declare 'assert_not_metatable'
-declare 'assert_error'
-declare 'assert_random'
-
-local TestMain =		require "test.TestMain"
 
 
-TestMain:Run();
+local gameConf	 = nil;
+local function GetConf()
+	if not gameConf and love.conf then
+		gameConf	 = {}
+		gameConf.screen  = {}
+		gameConf.modules = {}
+	
+		love.conf( gameConf );
+	end
+	
+	return gameConf;
+end
+
+
+
+local conf = GetConf()
+if conf and conf.test then
+	local TestMain = require("test.TestMain");
+	TestMain:Run();
+end
+
+
+
 
 --Shape affects physical traits - speed, weight, attack
 --Color and symbols affect special abilities
--- test/TestMain.lua
+++ test/TestMain.lua
@@ -1,5 +1,52 @@
 --===========================================================================--
---  Dependencies
+--  Dependencies (I)
+--===========================================================================--
+require 'src.strict';
+
+
+--===========================================================================--
+--  Forward Declarations
+--===========================================================================--
+-- lunatest dependencies
+declare 'random'
+declare 'fail'
+declare 'skip'
+declare 'assert_true'
+declare 'assert_false'
+declare 'assert_nil'
+declare 'assert_not_nil'
+declare 'assert_equal'
+declare 'assert_not_equal'
+declare 'assert_gt'
+declare 'assert_lt'
+declare 'assert_gte'
+declare 'assert_lte'
+declare 'assert_len'
+declare 'assert_not_len'
+declare 'assert_match'
+declare 'assert_not_match'
+declare 'assert_boolean'
+declare 'assert_not_boolean'
+declare 'assert_number'
+declare 'assert_not_number'
+declare 'assert_string'
+declare 'assert_not_string'
+declare 'assert_table'
+declare 'assert_not_table'
+declare 'assert_function'
+declare 'assert_not_function'
+declare 'assert_thread'
+declare 'assert_not_thread'
+declare 'assert_userdata'
+declare 'assert_not_userdata'
+declare 'assert_metatable'
+declare 'assert_not_metatable'
+declare 'assert_error'
+declare 'assert_random'
+
+
+--===========================================================================--
+--  Dependencies (II)
 --===========================================================================--
 require 'test.lib.lunatest.lunatest';
 
@@ -20,9 +67,7 @@ end
 --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
 --	TestMain : 
 --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
-local TestMain = 
-{
-}
+local TestMain = {}
 
 
 
@@ -58,6 +103,7 @@ function TestMain:Run()
 end;
 
 
-
-
+--===========================================================================--
+--  Initialization
+--===========================================================================--
 return TestMain;
