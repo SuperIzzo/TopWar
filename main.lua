b802f6a5dce1eec30211fdaba3da78c7ccfe6315
SuperIzzo <Hristoz2004@gmail.com>
2013-05-07 06:48:22 +0100

    An improved and faster normal map generator, a new arena and changed bits of the behaviour of the top

--- main.lua
+++ main.lua
@@ -77,7 +77,7 @@ function Top:new()
 	obj.topPhysics.y = 400;
 	obj.topPhysics.angle 		= 0;
 	obj.topPhysics.angleDelta 	= 0;
-	obj.topPhysics.angleVel 	= 0;
+	obj.topPhysics.angleVel 	= 340;
 	
 	return setmetatable(obj, self);
 end
@@ -118,7 +118,7 @@ end
 function Top:Update(dt)
 	local phTop = self.topPhysics;
 	
-	local angleAcc = 1*dt;
+	local angleAcc = -2*dt;
 	phTop.angleVel = phTop.angleVel + angleAcc;
 	
 	phTop.angleDelta = phTop.angleVel * dt;
@@ -127,107 +127,101 @@ end
 
 
 local sqrt = math.sqrt;
+
 --[[
+	This algorithm tries to be as efficient as possible, because the getPixel
+	routines seem to be quite expensive. First we compute the horizontal and
+	vertical gradients and store the results in `grad'. 
+	  ________________   The gradient computation uses the kernel shown left.
+	  | A0 | B0 | C0 |   C2 is the read cursor where the new color is obtained.
+	  | A1 | B1 | C1 |   B1 is the write cursor where the gradients are stored.
+	  | A2 | B2 | C2 |   The loop operates on both gradients (vertical and 
+	  ----------------   horizontal) simultaneously, but in order to do so has
+	     Fig. Kernel     to store information about previously read colors.
+						 The horizontal gradient is computed as (A1-C1)/2, the
+					     vertical as (B0-B2)/2.
+	Gausian blur is blur is later applied and the gradients are turned into 
+	color components.
+--]]
 local function MakeNormalMap( imgData, outData )
-	local w,h = imgData:getWidth(), imgData:getHeight();
-	local hr = {}
-	local vt = {}
+	local width, height = imgData:getWidth(), imgData:getHeight();	
 	
-	local prevXCol = imgData:getPixel( 0, 0 );
+	-- We need to store 2 columns of the original image
+	local columnA = {}
+	local columnB = {}
 	
-	local prevCol = {}
+	-- Get the first column in advance
+	for y = 0, height-1 do
+		columnA[y] = imgData:getPixel( 0, y );
+		columnB[y] = columnA[y];
+	end
 	
-	for pxX = 0, w-1 do
-		vt[pxX] = {}
-		hr[pxX] = {}
+	-- Store the gradients for later
+	local grad = {};
+	
+	for x = 0, height-2 do		
+		local C1 = imgData:getPixel( x, 0 );
+		columnB[-1] = columnB[0];
 		
-		local prevYCol = imgData:getPixel( pxX, 0 );
+		grad[x] = {};
 		
-		for pxY = 0, h-1 do
-			local col = imgData:getPixel( pxX, pxY );			
-			local prevXCol = prevCol[pxY] or col;			
+		for y = 0, height-2 do
+			local nextColor = imgData:getPixel( x+1, y+1 );
 			
-			vt[pxX][pxY] = col - prevYCol
-			hr[pxX][pxY] = col - prevXCol;
+			local hr = ( columnA[y] - C1 )/2;
+			local vr = ( columnB[y-1] - columnB[y+1] )/2;
 			
-			prevCol[pxY] = col;
-			prevYCol = col;
+			-- It is a 3D vector showing the direction of the normal
+			-- x and y are in the oposite direction of the gradients
+			-- while zet is implicitly 2
+			grad[x][y] = { hr, vr}
+			
+			-- Update colors
+			C1 = nextColor;
+			columnA[y] = columnB[y];
+			columnB[y] = C1;
 		end
 	end
 	
+	-- Aplly blur and compute and store as color
+	for x1=1, width-3 do
+		local x0 = x1-1;
+		local x2 = x1+1;
 		
-	for pxX = 0, w-2 do
-		for pxY = 0, h-2 do			
-			local dz = hr[pxX][pxY] + hr[pxX+1][pxY];
+		for y1=1, height-3 do
+			local y0 = y1-1;
+			local y2 = y1+1;
 			
-			local length = sqrt((dz/2)^2 +1);
-			local nx = -dz/length;
-			local nz = 1/length;
+			local nx = --grad[x1][y1][1] * 18
+			---[[
+				grad[x0][y0][1]   + grad[x1][y0][1]*2 + grad[x2][y0][1]   +
+				grad[x0][y1][1]*2 + grad[x1][y1][1]*4 + grad[x2][y1][1]*2 +
+				grad[x0][y2][1]   + grad[x1][y2][1]*2 + grad[x2][y2][1];
+			--]]
 			
-				  dz = vt[pxX][pxY] + vt[pxX][pxY+1];
+			local ny = --grad[x1][y1][2] * 18
+			---[[
+				grad[x0][y0][2]   + grad[x1][y0][2]*2 + grad[x2][y0][2] +
+				grad[x0][y1][2]*2 + grad[x1][y1][2]*4 + grad[x2][y1][2]*2 +
+				grad[x0][y2][2]   + grad[x1][y2][2]*2 + grad[x2][y2][2];
+			--]]
+							
+			-- after summing up the two vectors z becomes 2
+			-- nz^2 = (9 * 2*2)^2 = 36^2 = 1296
+			-- the first *2 is because of the gausian distribution
+			-- the second *2 because there is `nx' and `ny'
+			local len = sqrt(nx^2 + ny^2 + 1296)
 			
-				  length = sqrt((dz/2)^2 +1);
-			local ny = -dz/length;
-				  nz = nz + 1/length;
-			
-			
-			outData:setPixel( pxX, pxY, 127 + nx*127, 127 + ny*127, nz*127, 255 );
+			-- Compute the gradient vector and store in
+			outData:setPixel( x1, y1, 
+				nx/len * 127+127,
+				ny/len * 127+127,
+				36/len*255,
+				255);
 		end
 	end
 	
 end
---]]
-
----[[
-local function MakeNormalMap( imgData, outData )
-	local w,h = imgData:getWidth(), imgData:getHeight();
-	
-
-	local function saveGetPX( x, y )
-		if x< 0 then 
-			x = 0;
-		elseif x>= w then
-			x = w-1;
-		end
-		
-		if y< 0 then 
-			y = 0;
-		elseif y>= h then
-			y = h-1;
-		end
-		
-		return imgData:getPixel( x, y );
-	end
-	
-	local abs = math.abs
-	local function kernel(x, y)
-		local dz =imgData:getPixel( x, y );
-		
-		local nx, ny, nz = 0, 0, 0;
-		for xx= -2, 2 do
-			for yy= -2, 2 do
-				if xx ~=0 or yy ~= 0 then
-					local dzz = saveGetPX( x+xx, y+yy );
-									
-					nx = nx + xx*(dzz - dz);
-					ny = ny + yy*(dzz - dz);
-					nz = nz + sqrt(xx^2 + yy^2);
-				end
-			end			
-		end
-		
-		local len = sqrt(nx^2 + ny^2 + nz^2);
-		return nx/len*127+127, ny/len*127+127, nz/len*255;
-	end
-	
-	for pxX = 0, w-1 do
-		for pxY = 0, h-1 do
-			local r, g, b = kernel(pxX, pxY);
-			outData:setPixel( pxX, pxY, r, g, b, 255 );
-		end
-	end
-end
---]]
 
 
 local top1, top2;
@@ -244,7 +238,7 @@ function love.load()
 	arenaImg = love.graphics.newImage(arenaNMap);
 end
 
-local vx = 0;
+local vx = 30;
 local vy = 0;
 local fst = 5;
 function love.update(dt)
@@ -255,20 +249,20 @@ function love.update(dt)
 		fst = fst-1;
 	end
 	
-	local xd, yd = arenaNMap:getPixel( top1.topPhysics.x/4, top1.topPhysics.y/4 );
+	local xd, yd = arenaNMap:getPixel( top1.topPhysics.x, top1.topPhysics.y );
 	
-	vx = vx - vx*(0.0007*dt) + (xd/127 - 1)*dt*7;
-	vy = vy - vy*(0.0007*dt) + (yd/127 - 1)*dt*7;
+	vx = vx - vx*(0.007*dt) + (xd/127 - 1)*dt*50;
+	vy = vy - vy*(0.007*dt) + (yd/127 - 1)*dt*50;
 	
-	top1.topPhysics.x = top1.topPhysics.x - vx*dt*4;
-	top1.topPhysics.y = top1.topPhysics.y - vy*dt*4;
+	top1.topPhysics.x = top1.topPhysics.x + vx*dt*4;
+	top1.topPhysics.y = top1.topPhysics.y + vy*dt*4;
 	
 end
 
 function love.draw()
 	love.graphics.translate( -top1.topPhysics.x +love.graphics.getWidth()/2, -top1.topPhysics.y +love.graphics.getHeight()/2 );
 	love.graphics.setPixelEffect( nil );
-	love.graphics.draw( arenaImg, 0, 0, 0, 4, 4 );
+	love.graphics.draw( arenaImg, 0, 0, 0, 1, 1 );
 	top1:Draw();	
 end
 
