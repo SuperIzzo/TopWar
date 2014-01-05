467b1358a7e9c3228cd9af605f3adfc9269532f4
SuperIzzo <Hristoz2004@gmail.com>
2014-01-05 09:31:32 +0000

    A number of changes to the battle setup, including the ability gadget, the new arena constructor and the new ability.

--- src/scene/ScBattle.lua
+++ src/scene/ScBattle.lua
@@ -5,11 +5,13 @@ local Arena				= require 'src.object.Arena'
 local Dyzk				= require 'src.object.Dyzk'
 local Camera			= require 'src.object.Camera'
 local RPMMeter			= require 'src.object.RPMMeter'
+local AbilityGadget		= require 'src.object.AbilityGadget'
 local DBC				= require 'src.input.DirectBattleController'
 
 local SABoost			= require 'src.abilities.SABoost'
 local SARedirect		= require 'src.abilities.SARedirect'
 local SAReverseLeap		= require 'src.abilities.SAReverseLeap'
+local SAStone			= require 'src.abilities.SAStone'
 
 
 
@@ -17,6 +19,13 @@ local RPMCoords =
 {
 	[1] = { x=0,	y=0  },
 	[2] = { x=600,	y=0  }
+}
+
+
+local AbilityCoords = 
+{
+	[1] = { x=0,	y=25  },
+	[2] = { x=600,	y=25  }
 }
 
 --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
@@ -35,8 +44,11 @@ function ScBattle:new()
 	obj._dyzx = {};
 	obj._controllers = {}
 	obj._rpmMeters = {}
+	obj._abilityGadgets = {}
 	
-	obj._arena = Arena:new("data/arena/arena_mask2.png");
+	obj._arena = Arena:new(
+			"data/arena/arena_image2.png", 
+			"data/arena/arena_mask2.png" );
 	obj._arena:SetScale(4,4,8);
 	
 	obj._camera = Camera:new();
@@ -53,25 +65,27 @@ function ScBattle:Init()
 
 	-- Do some defaiult initialisation in case we have not been setup
 	if #self._dyzx==0 then
-		local dyzk1 = Dyzk:new("data/dyzx/DyzkAA002.png");
-		dyzk1.phDyzk.x = 100;
-		dyzk1.phDyzk.y = 100;
+		local dyzk1 = Dyzk:new("data/dyzx/DyzkAA006.png");
+		dyzk1.phDyzk.x = 1048;
+		dyzk1.phDyzk.y = 1048;
 		dyzk1.phDyzk.vx = 10;
 		dyzk1.phDyzk.vy = 10;
-		dyzk1.phDyzk.angVel = 600;
+		dyzk1.phDyzk.angVel = 10000/(dyzk1.phDyzk:GetWeight()^0.8 * dyzk1.phDyzk:GetMaxRadius()  /64);
 		dyzk1.phDyzk:SetAbility( 1, SARedirect:new( dyzk1.phDyzk ) );
 		dyzk1.phDyzk:SetAbility( 2, SABoost:new( dyzk1.phDyzk ) );
 		dyzk1.phDyzk:SetAbility( 3, SAReverseLeap:new( dyzk1.phDyzk ) );
+		dyzk1.phDyzk:SetAbility( 4, SAStone:new( dyzk1.phDyzk ) );
 		
 		local dyzk2 = Dyzk:new("data/dyzx/DyzkAA003.png");
 		dyzk2.phDyzk.x = 2048;
 		dyzk2.phDyzk.y = 2048;
 		dyzk2.phDyzk.vx = -10;
 		dyzk2.phDyzk.vy = -10;
-		dyzk2.phDyzk.angVel = 600;
+		dyzk2.phDyzk.angVel = 10000/(dyzk2.phDyzk:GetWeight()^0.8 * dyzk2.phDyzk:GetMaxRadius()  /64);
 		dyzk2.phDyzk:SetAbility( 1, SARedirect:new( dyzk2.phDyzk ) );
 		dyzk2.phDyzk:SetAbility( 2, SABoost:new( dyzk2.phDyzk ) );
 		dyzk2.phDyzk:SetAbility( 3, SAReverseLeap:new( dyzk2.phDyzk ) );
+		dyzk2.phDyzk:SetAbility( 4, SAStone:new( dyzk2.phDyzk ) );
 		
 		self._dyzx[1] = dyzk1;
 		self._dyzx[2] = dyzk2;
@@ -90,6 +104,12 @@ function ScBattle:Init()
 			self._rpmMeters[i] = 
 				RPMMeter:new( dyzk:GetPhysicsBody(), rpmCoord.x, rpmCoord.y );
 		end		
+		
+		local abilityCoord = AbilityCoords[i]
+		if abilityCoord then
+			self._abilityGadgets[i] = 
+				AbilityGadget:new( dyzk:GetPhysicsBody(), abilityCoord.x, abilityCoord.y );
+		end	
 	end
 end
 
@@ -115,7 +135,7 @@ function ScBattle:Update( dt )
 	
 	for i=1, #self._rpmMeters do
 		self._rpmMeters[i]:Update( dt );
-	end
+	end	
 end
 
 
@@ -134,6 +154,10 @@ function ScBattle:Draw()
 	
 	for i=1, #self._rpmMeters do
 		self._rpmMeters[i]:Draw();
+	end
+	
+	for i=1, #self._abilityGadgets do
+		self._abilityGadgets[i]:Draw();
 	end
 end
 
