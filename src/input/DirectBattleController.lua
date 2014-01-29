9151c774a7aa3af0691b1a623157d5fef1f71310
SuperIzzo <Hristoz2004@gmail.com>
2014-01-29 14:44:26 +0000

    A lot of changes... most imnportantly mouse handling

--- src/input/DirectBattleController.lua
+++ src/input/DirectBattleController.lua
@@ -14,11 +14,17 @@ BattleController.__index = BattleController;
 -------------------------------------------------------------------------------
 --  BattleController:new : Creates a new controller
 -------------------------------------------------------------------------------
-function BattleController:new( player, phDyzk )
+function BattleController:new( player, phDyzk, camera )
 	local obj = {}
 	
-	obj._dyzk	= phDyzk;
-	obj._player	= player;
+	obj._dyzk			= phDyzk;
+	obj._player			= player;
+	obj._controlDirX	= 0;	
+	obj._controlDirY	= 0;	
+	obj._pointX			= 0;
+	obj._pointY			= 0;	
+	obj._pointUpdate	= false;
+	obj._camera			= camera;
 	
 	return setmetatable( obj, self );
 end
@@ -28,7 +34,19 @@ end
 --  BattleController:Update : Updates the controller
 -------------------------------------------------------------------------------
 function BattleController:Update( dt )
-	-- DO NOTHING
+	if self._pointUpdate then
+		local pointX, pointY = 
+			self._camera:ScreenToWorld( self._pointX, self._pointY);
+		
+		local dyzkX, dyzkY = self._dyzk:GetPosition();
+				
+		self._controlDirX = pointX - dyzkX;
+		self._controlDirY = pointY - dyzkY;
+		
+		print( "Dyzk: ", dyzkX, dyzkY, "Point: ", pointX, pointY );
+	end
+	
+	self._dyzk:SetControlVector( self._controlDirX, self._controlDirY );
 end
 
 
@@ -36,34 +54,45 @@ end
 --  BattleController:Control : Handles controls
 -------------------------------------------------------------------------------
 function BattleController:Control( control )
-	if control.player == self._player then
-		if control.id == "xAxis" then
-			local val = control.value;
-			local vx, vy = self._dyzk:GetControlVector();
+	local player = control:GetBox().player;
+	
+	if player == self._player then
+		if control:GetID() == "xPoint" then
+			self._pointX = control:GetValue();
+		end
+		
+		if control:GetID() == "yPoint" then
+			self._pointY = control:GetValue();
+		end
+				
+		if control:GetID() == "Click" then
+			self._pointUpdate = control:GetValue();
 			
-			self._dyzk:SetControlVector( val, vy );
+			self._controlDirX = 0;
+			self._controlDirY = 0;
+		end		
+		
+		if control:GetID() == "xAxis" then
+			self._controlDirX = control:GetValue();			
 		end
 		
-		if control.id == "yAxis" then
-			local val = control.value;
-			local vx, vy = self._dyzk:GetControlVector();
-			
-			self._dyzk:SetControlVector( vx, val );
+		if control:GetID() == "yAxis" then
+			self._controlDirY = control:GetValue();
 		end
 		
-		if control.id == "A" then
-			self._dyzk:ActivateAbility( 1, control.value );
+		if control:GetID() == "A" then
+			self._dyzk:ActivateAbility( 1, control:GetValue() );
 		end
 		
-		if control.id == "B" then
+		if control:GetID() == "B" then
 			self._dyzk:ActivateAbility( 2, control.value );
 		end
 		
-		if control.id == "X" then
+		if control:GetID() == "X" then
 			self._dyzk:ActivateAbility( 3, control.value );
 		end
 		
-		if control.id == "Y" then
+		if control:GetID() == "Y" then
 			self._dyzk:ActivateAbility( 4, control.value );
 		end
 	end
