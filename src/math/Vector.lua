7ee6678def7489fe285f8039be955a9a871432be
SuperIzzo <Hristoz2004@gmail.com>
2014-02-23 08:32:32 +0000

    Vector now stores axes in its numeric keys rather than pretending to do so (this makes it faster)

--- src/math/Vector.lua
+++ src/math/Vector.lua
@@ -26,9 +26,9 @@ Vector.__remappedKeys = { x=1, y=2, z=3, w=4 }
 --  Vector:new : Creates a new vector instance
 -------------------------------------------------------------------------------
 function Vector:new( ... )
-	local obj = {}
+	local obj = {...}
 	
-	obj.data = {...};
+	obj.n = #obj;
 	
 	return setmetatable(obj, self);
 end
@@ -39,9 +39,9 @@ end
 -------------------------------------------------------------------------------
 function Vector:__index( key )
 
-	-- Vector axes can be accessed by numbers
+	-- Default value for numeric keys is 0 rather than nil
 	if type(key) == "number" then
-		return self.data[key] or 0;
+		return 0;
 	
 	-- If the data is an axis name we remap it
 	elseif type(key)== "string" then
@@ -60,10 +60,13 @@ end
 --  Vector:__index : defines the vector indexing
 -------------------------------------------------------------------------------
 function Vector:__newindex( key, value )
-
-	-- Vector axes can be accessed by numbers
-	if type(key) == "number" then
-		self.data[key] = value;
+	
+	-- If the data is a number check if we have to increase our dimension
+	if type(key)== "number" then
+		if (not self.n) or self.n<key then
+			self.n = key;
+			rawset( self, key, value );
+		end
 	
 	-- If the data is an axis name we remap it
 	elseif type(key)== "string" then
@@ -72,7 +75,7 @@ function Vector:__newindex( key, value )
 			self[remapedKey] = value;
 		end
 		
-	-- Finally, just set it
+	-- else, we just set it
 	else
 		rawset( self, key, value );
 	end
@@ -84,10 +87,10 @@ end
 -------------------------------------------------------------------------------
 function Vector:__add( other )
 	local result = Vector:new();
-	local len = max( #self.data, #other.data );
+	local len = max( #self, #other );
 	
 	for i=1, len do
-		result.data[i] = self[i] + other[i];
+		result[i] = self[i] + other[i];
 	end
 	
 	return result;
@@ -99,10 +102,10 @@ end
 -------------------------------------------------------------------------------
 function Vector:__sub( other )
 	local result = Vector:new();
-	local len = max( #self.data, #other.data );
+	local len = max( #self, #other );
 	
 	for i=1, len do
-		result.data[i] = self[i] - other[i];
+		result[i] = self[i] - other[i];
 	end
 	
 	return result;
@@ -115,8 +118,8 @@ end
 function Vector:__mul( scalar )
 	local result = Vector:new();
 	
-	for i=1, #self.data do
-		result.data[i] = self.data[i]*scalar;
+	for i=1, #self do
+		result[i] = self[i]*scalar;
 	end
 	
 	return result;
@@ -129,8 +132,8 @@ end
 function Vector:__div( scalar )
 	local result = Vector:new();
 	
-	for i=1, #self.data do
-		result.data[i] = self.data[i]/scalar;
+	for i=1, #self do
+		result[i] = self[i]/scalar;
 	end
 	
 	return result;
@@ -138,13 +141,21 @@ end
 
 
 -------------------------------------------------------------------------------
+--  Vector:__len : return dimension of the vector
+-------------------------------------------------------------------------------
+function Vector:__len()
+	return self.n;
+end
+
+
+-------------------------------------------------------------------------------
 --  Vector:__string : return text representation of the vector
 -------------------------------------------------------------------------------
 function Vector:__tostring()
-	local str = "Vector(" .. self.data[1];
+	local str = "Vector(" .. self[1];
 	
-	for i=2, #self.data do
-		str = str .. "," .. self.data[i];
+	for i=2, #self do
+		str = str .. "," .. self[i];
 	end
 	
 	return str..")";
@@ -155,10 +166,10 @@ end
 --  Vector:Length : vector length
 -------------------------------------------------------------------------------
 function Vector:Length()
-	local squareSum = self.data[1]^2;
+	local squareSum = self[1]^2;
 	
-	for i=2, #self.data do
-		squareSum = squareSum + self.data[i]^2;
+	for i=2, #self do
+		squareSum = squareSum + self[i]^2;
 	end
 	
 	return squareSum^0.5;
@@ -169,11 +180,11 @@ end
 --  Vector:Dot : vector dot product
 -------------------------------------------------------------------------------
 function Vector:Dot( other )
-	local len = min( #self.data, #other.data );
+	local len = min( #self, #other );
 	local dot = 0;
 	
 	for i=1, len do
-		dot = dot + self.data[i] * other.data[i];
+		dot = dot + self[i] * other[i];
 	end
 	
 	return dot;
@@ -188,8 +199,8 @@ function Vector:Unit()
 	local len = self:Length();
 	
 	if len>0 then
-		for i=1, #self.data do
-			result.data[i] = self.data[i]/len;
+		for i=1, #self do
+			result[i] = self[i]/len;
 		end
 	end
 	
