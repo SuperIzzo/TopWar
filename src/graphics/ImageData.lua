b9d20d0534d8171f5805afc7722a9b37df39e1cb
SuperIzzo <Hristoz2004@gmail.com>
2014-03-03 07:47:26 +0000

    ImageData data now uses Array2D. It can be constructed from raw data. Added grayscale image data support. Implemented ImageData.copy

--- src/graphics/ImageData.lua
+++ src/graphics/ImageData.lua
@@ -1,7 +1,7 @@
 --===========================================================================--
 --  Dependencies
 --===========================================================================--
-
+local Array2D			= require 'src.util.Array2D'
 
 
 --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
@@ -14,12 +14,14 @@ ImageData.__index = ImageData;
 -------------------------------------------------------------------------------
 --  ImageData:new : Creates a new ImageData
 -------------------------------------------------------------------------------
-function ImageData:new( width, height)
+function ImageData:new( width, height )
 	local obj = {}
-	
-	obj.width	= width; 
-	obj.height	= height;
-	obj.pixels	= {};
+
+	if type(width) == "number" then
+		obj.data = Array2D:new( width, height )
+	elseif type(width) == "table" then
+		obj.data = width;
+	end
 
 	return setmetatable(obj, self);
 end
@@ -29,7 +31,7 @@ end
 --  ImageData:getHeight : Returns the height of the image
 -------------------------------------------------------------------------------
 function ImageData:getHeight()
-	return self.height;
+	return self.data:GetHeight();
 end
 
 
@@ -37,7 +39,7 @@ end
 --  ImageData:getWidth : Returns the height of the image
 -------------------------------------------------------------------------------
 function ImageData:getWidth()
-	return self.width;
+	return self.data:GetWidth();
 end
 
 
@@ -45,19 +47,7 @@ end
 --  ImageData:getDimensions : Returns the width and height of the image
 -------------------------------------------------------------------------------
 function ImageData:getDimensions()
-	return self.width, self.height;
-end
-
-
--------------------------------------------------------------------------------
---  ImageData:getPixel : Returns the height of the image
--------------------------------------------------------------------------------
-function ImageData:_getCoordsIndex(x,y)
-	if x<0 or x>=self.width or y<0 or y>=self.height then
-		error( "Pixel out of bounds" );
-	end
-	
-	return x + y*self.width;
+	return self:getWidth(), self:getHeight();
 end
 
 
@@ -65,11 +55,12 @@ end
 --  ImageData:getPixel : Returns the height of the image
 -------------------------------------------------------------------------------
 function ImageData:getPixel(x,y)
-	local pixelIdx = self:_getCoordsIndex(x,y)
-	local pixel = self.pixels[pixelIdx];
+	local pixel = self.data:Get( x, y );
 	
-	if pixel then
-		return pixel[1], pixel[2], pixel[3], pixel[4];
+	if type(pixel) == "table" then
+		return pixel[1], pixel[2], pixel[3], (pixel[4] or 255);
+	elseif type(pixel) == "number" then
+		return pixel, pixel, pixel, 255;
 	else
 		return 0,0,0,0;
 	end
@@ -80,15 +71,18 @@ end
 --  ImageData:setPixel : Sets a pixel in the image
 -------------------------------------------------------------------------------
 function ImageData:setPixel(x,y, r,g,b,a)
-	local pixelIdx = self:_getCoordsIndex(x,y)
-	local pixel = self.pixels[pixelIdx] or {};
+	local pixel = self.data:Get( x, y )
 	
-	pixel[1] = r;
-	pixel[2] = g;
-	pixel[3] = b;
-	pixel[4] = a or 255;
-	
-	self.pixels[pixelIdx] = pixel;
+	if type(pixel) == "table" then
+		pixel[1] = r;
+		pixel[2] = g;
+		pixel[3] = b;
+		pixel[4] = a;
+	elseif type(pixel) == "number" then
+		self.data:Set( x, y, r );	
+	else
+		self.data:Set( x, y, { r, g, b, a } );
+	end	
 end
 
 
@@ -96,8 +90,12 @@ end
 --  ImageData:copy : Copies the image to another image
 -------------------------------------------------------------------------------
 function ImageData.copy( source, dest, dx, dy, sx, sy, sw, sh )
-	for x = 1, sw do
-		for y = 1, sh do
+	local dx, dy = (dx or 0), (dy or 0);
+	local sx, sy = (sx or 0), (sy or 0); 
+	local sw, sh = (sw or source:getWidth()), (sh or source:getHeight());
+	
+	for x = 0, sw-1 do
+		for y = 0, sh-1 do
 			dest:setPixel( dx+x, dy+y, source:getPixel(sx+x, sy+y) );
 		end
 	end
