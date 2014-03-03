dadc9257b801382f00f7cd539ebfaa5ace9df575
SuperIzzo <Hristoz2004@gmail.com>
2014-03-03 08:24:09 +0000

    Settings: Added joystick mappings again

--- src/settings/Settings.lua
+++ src/settings/Settings.lua
@@ -85,6 +85,8 @@ function Settings:LoadDefaultControls( box )
 		-- Setup the triggers (some can be reused on multiple controls)
 		local digitalAxisX 			= DigitalAxis:new();
 		local digitalAxisY 			= DigitalAxis:new();
+		local digitalJoyAxisX		= DigitalAxis:new();
+		local digitalJoyAxisY		= DigitalAxis:new();
 		local negAxisTrigger		= AnalogueButton:new( -2, -0.5 );
 		local posAxisTrigger		= AnalogueButton:new( 0.5,  2 );
 		local anlgAxisTrigger		= AnalogueAxis:new()
@@ -92,17 +94,32 @@ function Settings:LoadDefaultControls( box )
 		
 		digitalAxisX:SetOffValue(0)
 		digitalAxisY:SetOffValue(0)
-		
+		digitalJoyAxisX:SetOffValue(0)
+		digitalJoyAxisY:SetOffValue(0)
 		
 		local xAxis = box:CreateControl("xAxis");
 		xAxis:SetValue(0);
 		xAxis:Bind'Key'( 'a', digitalAxisX:TriggerOn(-1) );
 		xAxis:Bind'Key'( 'd', digitalAxisX:TriggerOn( 1) );
+		xAxis:Bind'Joy1Axis'( 1, anlgAxisTrigger:Trigger() );
+		xAxis:Bind'Joy1Hat'( 'l', digitalJoyAxisX:TriggerOn(-1) );
+		xAxis:Bind'Joy1Hat'( 'ld', digitalJoyAxisX:TriggerOn(-1) );
+		xAxis:Bind'Joy1Hat'( 'lu', digitalJoyAxisX:TriggerOn(-1) );
+		xAxis:Bind'Joy1Hat'( 'r', digitalJoyAxisX:TriggerOn( 1) );
+		xAxis:Bind'Joy1Hat'( 'ru', digitalJoyAxisX:TriggerOn( 1) );
+		xAxis:Bind'Joy1Hat'( 'rd', digitalJoyAxisX:TriggerOn( 1) );
 
 		local yAxis	= box:CreateControl("yAxis");
 		yAxis:SetValue(0);
 		yAxis:Bind'Key'( 'w', digitalAxisY:TriggerOn(-1) );
 		yAxis:Bind'Key'( 's', digitalAxisY:TriggerOn( 1) );
+		yAxis:Bind'Joy1Axis'( 2, anlgAxisTrigger:Trigger() );
+		yAxis:Bind'Joy1Hat'( 'u', digitalJoyAxisY:TriggerOn(-1) );
+		yAxis:Bind'Joy1Hat'( 'lu', digitalJoyAxisY:TriggerOn(-1) );
+		yAxis:Bind'Joy1Hat'( 'ru', digitalJoyAxisY:TriggerOn(-1) );
+		yAxis:Bind'Joy1Hat'( 'd', digitalJoyAxisY:TriggerOn( 1) );
+		yAxis:Bind'Joy1Hat'( 'ld', digitalJoyAxisY:TriggerOn( 1) );
+		yAxis:Bind'Joy1Hat'( 'rd', digitalJoyAxisY:TriggerOn( 1) );
 		
 		local Left				= box:CreateControl("Left");
 		Left:SetValue( false );
@@ -137,18 +154,22 @@ function Settings:LoadDefaultControls( box )
 		local A					= box:CreateControl( "A" )
 		A:SetValue( false );
 		A:Bind'Key'( 'kp1',				binButtonTrigger:Trigger()	);
+		A:Bind'Joy1Button'( 3,			binButtonTrigger:Trigger()	);
 		
 		local B					= box:CreateControl( "B" )
 		B:SetValue( false );
 		B:Bind'Key'( 'kp2',				binButtonTrigger:Trigger()	);
+		B:Bind'Joy1Button'( 2,			binButtonTrigger:Trigger()	);
 		
 		local X					= box:CreateControl( "X" )
 		X:SetValue( false );
 		X:Bind'Key'( 'kp3',				binButtonTrigger:Trigger()	);
+		X:Bind'Joy1Button'( 4,			binButtonTrigger:Trigger()	);
 		
 		local Y					= box:CreateControl( "Y" )
 		Y:SetValue( false );
 		Y:Bind'Key'( 'kp4',				binButtonTrigger:Trigger()	);
+		Y:Bind'Joy1Button'( 1,			binButtonTrigger:Trigger()	);
 
 	--[[
 	local xAxis	= box:CreateControl("xAxis");
