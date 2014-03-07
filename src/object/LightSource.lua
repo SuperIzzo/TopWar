7dcc24ab67d0a98eaee95df39b9be266c362a67c
SuperIzzo <Hristoz2004@gmail.com>
2014-03-07 18:52:49 +0000

    Light is now called LightSource and is located in "object" Lights takes the role of a light manager and is responsible for the lighting shader now

---
+++ src/object/LightSource.lua
@@ -0,0 +1,209 @@
+--===========================================================================--
+--  Dependencies
+--===========================================================================--
+
+
+
+--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
+--	Class LightSource: a brief... 
+--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
+local LightSource = {}
+LightSource.__index = LightSource;
+
+
+-------------------------------------------------------------------------------
+--  LightSource:new : Creates a new LightSource
+-------------------------------------------------------------------------------
+function LightSource:new( lightType, color, diffuseStr, ambientStr, ... )
+	local obj = {}
+	
+	local arg = {...};
+	
+	obj._type	= lightType;
+	obj._on		= true;
+	obj._col	= color or {255,255,255,255};
+	obj._dif	= diffuseStr or 1;
+	obj._amb	= ambientStr or 0;
+	
+	if lightType == "directional" then
+		obj._dir	= arg[1] or {0,0,-1}		
+	elseif lightType == "point" then
+		obj._pos	= arg[1] or {0,0,0};		
+		obj._atten = 
+		{
+			const	= arg[2] or 0;
+			linear	= arg[3] or 0;
+			exp 	= arg[4] or 1;
+		}
+	else
+		error( "Invalid light type: " .. tostring(lightType) );
+	end		
+
+	return setmetatable(obj, self);
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:Send : Sends the light to the given shader
+-------------------------------------------------------------------------------
+function LightSource:Send( shader, idx )
+	local lightPrefix = self._type .. "Lights[" .. idx .. "].";
+	shader:send( lightPrefix .. "base.color",
+		{ 
+			(self._col[1] or 0)		/ 255,
+			(self._col[2] or 0)		/ 255,
+			(self._col[3] or 0)		/ 255,
+			(self._col[4] or 255)	/ 255,
+		}
+	);			
+	
+	shader:send( lightPrefix .. "base.ambientStrength", self._amb );
+	shader:send( lightPrefix .. "base.diffuseStrength", self._dif );
+	
+	if self._type == "directional" then
+		shader:send( lightPrefix .. "direction", self._dir );
+	elseif self._type == "point" then
+		shader:send( lightPrefix .. "position", self._pos );
+		shader:send( lightPrefix .. "attenConst", self._atten.const );
+		shader:send( lightPrefix .. "attenLinear", self._atten.linear );
+		shader:send( lightPrefix .. "attenExp", self._atten.exp );
+	end
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:GetType : Returns the type of the light source
+-------------------------------------------------------------------------------
+function LightSource:GetType()
+	return self._type;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:IsOn : Returns whether the light source is on
+-------------------------------------------------------------------------------
+function LightSource:IsOn()
+	return self._on;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:SetOn : Turns the light source on/off
+-------------------------------------------------------------------------------
+function LightSource:SetOn( on )
+	self._on = on;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:SetColor : Sets the color of the light source
+-------------------------------------------------------------------------------
+function LightSource:SetColor( r, g, b, a )
+	assert( r and g and b );
+	self._col = { r, g, b, a or 255 };
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:GetColor : Returns the color of the light source
+-------------------------------------------------------------------------------
+function LightSource:GetColor()
+	return self._col[1], self._col[2], self._col[3], self._col[4];
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:SetDiffuseStrength : Sets the diffuse strength of the light
+-------------------------------------------------------------------------------
+function LightSource:SetDiffusePower( str )
+	self._dif = str;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:GetDiffusePower : Returns the diffuse strength of the light
+-------------------------------------------------------------------------------
+function LightSource:GetDiffusePower()
+	return self._dif;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:SetAmbientPower : Sets the diffuse strength of the light
+-------------------------------------------------------------------------------
+function LightSource:SetAmbientPower( str )
+	self._dif = str;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:GetAmbientPower : Returns the ambient strength of the light
+-------------------------------------------------------------------------------
+function LightSource:GetAmbientPower()
+	return self._amb;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:SetDirection : Sets the direction of a light source
+-------------------------------------------------------------------------------
+function LightSource:SetDirection( x, y, z )
+	assert( self._type == "directional" );
+	assert( x and y and z );
+	self._dir = {x,y,z}
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:GetDirection : Returns the direction of a light source
+-------------------------------------------------------------------------------
+function LightSource:GetDirection()
+	assert( self._type == "directional" );
+	return self._dir[1], self._dir[2], self._dir[3]
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:SetPosition : Sets the position of a point light source
+-------------------------------------------------------------------------------
+function LightSource:SetPosition( x, y, z )
+	assert( self._type == "point" );
+	assert( x and y and z );
+	self._pos = {x,y,z}
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:GetPosition : Returns the attenuation of a point light source
+-------------------------------------------------------------------------------
+function LightSource:GetPosition()
+	assert( self._type == "point" );
+	return self._pos[1], self._pos[2], self._pos[3]
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:SetAttenuation : Sets the attenuation of a point light source
+-------------------------------------------------------------------------------
+function LightSource:SetAttenuation( const, linear, exp )
+	assert( self._type == "point" );
+	self._atten.const	= const		or 0;
+	self._atten.linear	= linear	or 0;
+	self._atten.exp 	= exp		or 0;
+end
+
+
+-------------------------------------------------------------------------------
+--  LightSource:GetAttenuation : Returns the attenuation of a point light
+-------------------------------------------------------------------------------
+function LightSource:GetAttenuation()
+	assert( self._type == "point" );
+	return	self._atten.const, self._atten.linear, self._atten.exp;
+end
+
+
+
+--===========================================================================--
+--  Initialization
+--===========================================================================--
+return LightSource
---
+++ src/object/Lights.lua
@@ -0,0 +1,146 @@
+--===========================================================================--
+--  Dependencies
+--===========================================================================--
+local LightSource			= require 'src.object.LightSource'
+local Array					= require 'src.util.Array'
+
+
+
+--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
+--	Class Lights: a brief... 
+--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
+local Lights = {}
+Lights.__index = Lights;
+
+
+-------------------------------------------------------------------------------
+--  Lights:new : Creates a new Lights
+-------------------------------------------------------------------------------
+function Lights:new()
+	local obj = {}
+	
+	obj._dirLights = Array:new()
+	obj._pointLights = Array:new()
+	
+	-- Init class variables
+	self:_SetupShaders();
+
+	return setmetatable(obj, self);
+end
+
+
+-------------------------------------------------------------------------------
+--  Lights:_SetupShaders : Sets up shaders
+-------------------------------------------------------------------------------
+function Lights:_SetupShaders()
+	-- Skip this if we are initialized
+	if self._lightingShader then
+		return;
+	end
+	
+	if love.graphics.isSupported( "shader" ) then
+		local shaderCode = love.filesystem.read( "data/gfx/shaders/lighting.frag" );
+		local ok, shader = pcall( love.graphics.newShader, shaderCode );
+		
+		if ok then
+			self._lightingShader = shader;
+		else
+			error( shader );
+		end		
+	end
+end
+	
+	
+-------------------------------------------------------------------------------
+--  Lights:CreatePointLight : Creates a point light source
+-------------------------------------------------------------------------------
+function Lights:CreatePointLight(...)
+	local light = LightSource:new( "point", ... );
+	self._pointLights:Add( light );
+	
+	return light;
+end
+
+
+-------------------------------------------------------------------------------
+--  Lights:CreateDirectionalLight : Creates a directional light source
+-------------------------------------------------------------------------------
+function Lights:CreateDirectionalLight( ... )
+	local light = LightSource:new( "directional", ... );
+	self._dirLights:Add( light );
+	
+	return light;
+end
+
+
+-- HACK! -- should be solved by properly managing light resources
+-- Rather than deleting them
+local offDirLight = LightSource:new( "directional", {0,0,0,0} );
+local offPointLight = LightSource:new( "point", {0,0,0,0} );
+-------------------------------------------------------------------------------
+--  Lights:Send : Sends the light sources to the shader
+-------------------------------------------------------------------------------
+function Lights:SendLights( shader )
+	local MAX_DIR_LIGHTS = 3;
+	local MAX_POINT_LIGHTS = 3;
+	
+	local shader = shader or self._lightingShader;
+	if not shader then
+		return;
+	end
+	
+	local numDirLights = 0;
+	for dirLight in self._dirLights:Items() do
+		if dirLight and dirLight:IsOn() then
+			dirLight:Send( shader, numDirLights ); 
+			
+			numDirLights = numDirLights+1;
+			if numDirLights>= MAX_DIR_LIGHTS then
+				break;
+			end
+		end
+	end
+	
+	for i = numDirLights, MAX_DIR_LIGHTS-1 do
+		offDirLight:Send( shader, i );
+	end
+	
+	local numPointLights = 0;
+	for pointLight in self._pointLights:Items() do
+		if pointLight and pointLight:IsOn() then 
+			pointLight:Send( shader, numPointLights );
+			
+			numPointLights = numPointLights+1;			
+			if numPointLights>= MAX_POINT_LIGHTS then
+				break;
+			end
+		end
+	end
+	
+	for i = numPointLights, MAX_POINT_LIGHTS-1 do
+		offPointLight:Send( shader, i );
+	end
+end
+
+
+-------------------------------------------------------------------------------
+--  Lights:Use : Sends the light sources to the shader
+-------------------------------------------------------------------------------
+function Lights:Use( normalMap )
+	if self._lightingShader then
+		if normalMap then
+			love.graphics.setShader( self._lightingShader );
+			self._lightingShader:send( "normalMap", normalMap );
+			self:SendLights();
+		else
+			love.graphics.setShader( nil );
+		end
+	end
+	
+end
+
+
+--===========================================================================--
+--  Initialization
+--===========================================================================--
+return Lights
