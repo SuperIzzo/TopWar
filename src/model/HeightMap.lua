069c3c285e7940c960e1f87a48fd9b87d2871122
SuperIzzo <Hristoz2004@gmail.com>
2014-03-07 18:57:14 +0000

    NormalMap and HeightMap now bilerp moving the clutter away from ArenaModel

--- src/model/HeightMap.lua
+++ src/model/HeightMap.lua
@@ -3,11 +3,16 @@
 --===========================================================================--
 local Byter				= require 'src.util.Byter'
 local Array2D			= require 'src.util.Array2D'
+local MathUtils 		= require 'src.math.MathUtils'
 
+local clamp				= MathUtils.Clamp;
+local bilerp			= MathUtils.Bilerp;
+local floor				= math.floor;
 local uint2bytes		= Byter.UIntegerToBytes
 local bytes2uint		= Byter.BytesToUInteger
 local double2bytes		= Byter.DoubleToBytes
 local bytes2double		= Byter.BytesToDouble
+
 
 --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
 --	Class HeightMap : a brief... 
@@ -40,7 +45,29 @@ end
 --  HeightMap:Get : Returns the depth at x, y
 -------------------------------------------------------------------------------
 function HeightMap:Get(x, y)
-	return self.map:Get(x, y);
+	local width = self.map:GetWidth();
+	local height = self.map:GetHeight();
+	
+	if x>=0 and x<=width-2 and y>=0 and y<=height-2 then
+		local ax, ay = floor(x), floor(y);
+		local bx, by = ax+1, ay+1;
+		local t1, t2 = x - ax, y - ay;
+		
+		if t1 == 0 and t2 == 0 then
+			return self.map:Get(ax,ay);
+		else					
+			local depth00 = self.map:Get(ax,ay) or 0;
+			local depth01 = self.map:Get(ax,by) or 0;
+			local depth10 = self.map:Get(bx,ay) or 0;
+			local depth11 = self.map:Get(bx,by) or 0;
+			
+			local depth = bilerp( depth00, depth01, depth10, depth11, t1, t2 );
+			
+			return depth;
+		end
+	else
+		return 0;
+	end
 end
 
 
--- src/model/NormalMap.lua
+++ src/model/NormalMap.lua
@@ -2,8 +2,14 @@
 --  Dependencies
 --===========================================================================--
 local Array2D			= require 'src.util.Array2D'
+local MathUtils 		= require 'src.math.MathUtils'
 local ImageData			= require 'src.graphics.ImageData'
 local ImageUtils		= require 'src.graphics.ImageUtils'
+
+local clamp				= MathUtils.Clamp;
+local bilerp			= MathUtils.Bilerp;
+local floor				= math.floor
+local ceil				= math.ceil
 
 
 --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--
@@ -24,18 +30,56 @@ end
 
 
 -------------------------------------------------------------------------------
---  NormalMap:Get : Returns the depth at x, y
--------------------------------------------------------------------------------
-function NormalMap:Get(x, y)
-	return self.map:Get(x, y);
-end
-
-
--------------------------------------------------------------------------------
 --  NormalMap:Set : Sets the depth at x, y
 -------------------------------------------------------------------------------
 function NormalMap:Set(x, y, vec)
 	return self.map:Set(x, y, vec);
+end
+
+
+-------------------------------------------------------------------------------
+--  NormalMap:Get : Returns the depth at x, y
+-------------------------------------------------------------------------------
+function NormalMap:Get(x, y)
+	local width = self.map:GetWidth();
+	local height = self.map:GetHeight();
+	
+	if x>=0 and x<=width-2 and y>=0 and y<=height-2 then
+		local ax, ay = floor(x), floor(y);
+		local bx, by = ax+1, ay+1;
+		local t1, t2 = x - ax, y - ay;
+		
+		if t1 == 0 and t2 == 0 then
+			return self.map:Get(ax,ay);
+		else					
+			local n00_x, n00_y, n00_z = unpack( self.map:Get(ax,ay) or {0,0,0} );
+			local n01_x, n01_y, n01_z = unpack( self.map:Get(ax,by) or {0,0,0} );
+			local n10_x, n10_y, n10_z = unpack( self.map:Get(bx,ay) or {0,0,0} );
+			local n11_x, n11_y, n11_z = unpack( self.map:Get(bx,by) or {0,0,0} );
+			
+			local nx = bilerp( n00_x, n01_x, n10_x, n11_x, t1, t2 );
+			local ny = bilerp( n00_y, n01_y, n10_y, n11_y, t1, t2 );
+			local nz = bilerp( n00_z, n01_z, n10_z, n11_z, t1, t2 );
+			
+			return {nx, ny, nz}
+		end
+	else
+		return {127,127,0};
+	end
+end
+
+
+-------------------------------------------------------------------------------
+--  NormalMap:Get : Returns the depth at x, y
+-------------------------------------------------------------------------------
+function NormalMap:GetNormal(x, y)
+	local normal = self:Get( x, y );
+	
+	normal[1] = (normal[1] - 127)/255;
+	normal[2] = (normal[2] - 127)/255;
+	normal[3] = (   normal[3]   )/255;
+	
+	return normal;
 end
 
 
